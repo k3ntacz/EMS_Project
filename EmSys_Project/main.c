52e6bd1046a6fa3232bdc3d9ab4b69bdbe81ce2f
Petr <petrvass10@gmail.com>
2025-10-07 15:55:56 +0200

    Initial Commit

---
+++ EmSys_Project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  EmSys_Project
+//
+//  Created by Petr - pracovní on 07.10.2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
